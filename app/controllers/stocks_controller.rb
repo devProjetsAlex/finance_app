16b5693a688bfde270682eec5cadb3312461bf54
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-04-28 12:55:20 -0400

    result partial

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,9 +2,12 @@ class StocksController < ApplicationController
 
     def search
         if params[:stock].present?            
-        @stock = Stock.new_lookup(params[:stock])
+          @stock = Stock.new_lookup(params[:stock])
             if @stock
-        render 'users/my_portfolio'
+            respond_to do |format|
+            format.js {render partial: 'users/result'}
+            end
+
             else
             flash[:alert] = 'Please enter a valid stock symbol to search'
             redirect_to my_portfolio_path
