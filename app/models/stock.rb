ccf03adb20de4ca63ab816ce4236ce0c8d5041f8
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-04-30 14:45:30 -0400

    api key mod

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,8 +6,8 @@ class Stock < ApplicationRecord
   validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
-  client = IEX::Api::Client.new(publishable_token: ENV["iex_api_key"],
-    endpoint: 'https://cloud.iexapis.com/'
+  client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+    endpoint: 'https://sandbox.iexapis.com/v1'
 )
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol) )
